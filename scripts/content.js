08cfc85840249e23509f8a4c3f377ce3c7a54b27
karinazhuro <kzhuro36@gmail.com>
2024-01-08 16:17:01 +0100

    implement play/pause video

--- scripts/content.js
+++ scripts/content.js
@@ -1,29 +1,76 @@
 'use strict';
 
-const article = document.querySelector("article");
+function onClickPlayButton() {
+		const player = document.getElementById('oframecdnplayer');
+		const buttonPlayer = getPjsdiv(player)[79];
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
+		const itemsForPlay = [player, buttonPlayer];
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+		itemsForPlay.forEach((item) => item.addEventListener('click', () => {
+				getPlayerControlTimelinePassed();
+		}));
 }
 
-const components = document.getElementsByTagName('pjsdiv');
-const containerSubtitles = components[components.length - 1];
+function getPlayerControlTimelinePassed() {
+		const playerControlTimelinePassed = getPjsdiv(player)[23];
 
-// containerSubtitles.textContent = 'pjsdiv';
+		checkStyleLeft(playerControlTimelinePassed);
+}
+
+function checkStyleLeft(control) {
+		if(+control.style.width.slice(0, -2) > 0) {
+				getSubtitles();
+		}
+}
+
+function getSubtitles() {
+		const components = document.getElementsByTagName('pjsdiv');
+		const containerSubtitles = components[components.length - 1];
+		// const videoPlayer = document.getElementsByTagName('video')[0];
+
+		containerSubtitles.classList.add('subtitles');
+
+		containerSubtitles.addEventListener('mouseover', () => {
+				// videoPlayer.pause();
+		});
+
+		containerSubtitles.addEventListener('mouseout', () => {
+				// videoPlayer.play().catch(error => {
+				// 		console.error('Ошибка воспроизведения видео:', error);
+				// });
+		});
+
+		console.log('getSubtitles');
+
+		containerSubtitles.addEventListener('mousedown', (e) => {
+				console.log(e);
+
+				containerSubtitles.style.left = '0px';
+				containerSubtitles.style.top = '85%';
+		});
+
+		// containerSubtitles.addEventListener('mousedown', function(e) {
+				// var offsetX = e.clientX - subtitles.getBoundingClientRect().left;
+				// var offsetY = e.clientY - subtitles.getBoundingClientRect().top;
+				//
+				// function mouseMoveHandler(e) {
+				// 		subtitles.style.left = (e.clientX - offsetX) + 'px';
+				// 		subtitles.style.top = (e.clientY - offsetY) + 'px';
+				// }
+				//
+				// function reset() {
+				// 		window.removeEventListener('mousemove', mouseMoveHandler);
+				// 		window.removeEventListener('mouseup', reset);
+				// }
+				//
+				// window.addEventListener('mousemove', mouseMoveHandler);
+				// window.addEventListener('mouseup', reset);
+		// });
+}
+
+function getPjsdiv(selector) {
+		return selector.querySelectorAll('pjsdiv');
+}
+
+onClickPlayButton();
+
